70f46d58cbb2df295727b34ddf16eefdc2cb9d4a
Jack Cohen <thejackcohen@gmail.com>
2020-06-16 23:37:40 -0400

    Idempotency in trigger method

--- src/functions/trigger/index.js
+++ src/functions/trigger/index.js
@@ -1,6 +1,20 @@
 import { DynamoDB } from 'services/AWS'
 import { deleteMessage } from 'services/SQS'
 
+const getEvent = async ({ userId, eventId }) => {
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+            pk: `USER#${userId}`,
+            sk: `EVENT#${eventId}`,
+        },
+        ConsistentRead: true,
+    }
+
+    const result = await DynamoDB.get(params).promise()
+    return result.Item
+}
+
 const markSent = async ({ userId, eventId, sentTime }) => {
     const params = {
         TableName: process.env.DYNAMODB_TABLE,
@@ -34,13 +48,16 @@ export const handler = async event => {
         } = r
 
         try {
-            // TODO Send webhook
-            console.log('Sending webhook with data...', body)
-            await markSent({
-                userId,
-                eventId,
-                sentTime: new Date().toISOString(),
-            })
+            const event = await getEvent({ userId, eventId })
+            if (!event.data.sentTime) {
+                // TODO Send webhook
+                console.log('Sending webhook with body...', body)
+                await markSent({
+                    userId,
+                    eventId,
+                    sentTime: new Date().toISOString(),
+                })
+            }
             await deleteMessage({ receiptHandle })
         } catch (e) {
             console.error('Handling error...', e)
